e9d864af573ed19e976f740abf1b48588818980f
Vasile Cotovanu <vasile.cotovanu@local.ch>
2014-02-12 22:59:06 +0100

    Helper belongs to gtfs.rb

--- inc/gtfs.rb
+++ inc/gtfs.rb
@@ -230,4 +230,20 @@ class GTFS
     routes = @db.execute(sql)
     return routes
   end
+
+# Adapted from https://gist.github.com/j05h/673425
+#   loc1 and loc2 are arrays of [latitude, longitude]
+def compute_distance (lon1, lat1, lon2, lat2)
+  def deg2rad(deg)
+    return deg * Math::PI / 180
+  end
+
+  dLat = deg2rad(lat2-lat1)
+  dLon = deg2rad(lon2-lon1)
+  a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+  c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+  d = (6371 * c * 1000).to_i
+end
 end
